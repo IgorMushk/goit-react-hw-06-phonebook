cbc4ab7766e2726ebfc5e4892f5f67fdf4b48809
IgorMushk <igor.mushketik@gmail.com>
2023-10-06 20:27:31 +0300

    Add lib Redux Persist - del comments in store.js
    del comments in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,59 +1,10 @@
-//import { configureStore, nanoid } from '@reduxjs/toolkit';
 import { configureStore } from '@reduxjs/toolkit';
-//import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
-//import { createSlice } from "@reduxjs/toolkit";
 import { filterReducer } from './filterSlice';
 import { contactReducer } from './contactsSlice';
-//import contacts from '../data/contacts.json';
-
-//console.log(contacts)
-
-// const contactsSlice = createSlice({
-//   name: 'clients',
-//   initialState: contacts,
-//   reducers:{
-//     addClient(state,action) {
-//       return [...state, action.payload]
-//     },
-//     delClient(state,action) {
-//       return state.filter(contact => contact.id !== action.payload)
-//     },
-//   }
-// })
-// console.log('contactsSlice >>', contactsSlice);
-
-// const filterSlice = createSlice({
-//   name: 'filter',
-//   initialState: '',
-//   reducers: {
-//     setFilter(tate,action) {
-//       return action.payload
-//     }
-//   }
-// })
-
-// export const addClient = createAction("clients/addClient");
-// export const delClient = createAction("clients/delClient");
-// export const setFilter = createAction("filter/setFilter");
-
-//console.log(addClient({name: 'Имя клиента', number: '11111111111' }));
-
-// const contactReducer = createReducer(contacts, {
-//   [addClient]: (state, action) => {return [...state, action.payload];},
-//   [delClient]: (state, action) => {return state.filter(contact => contact.id !== action.payload);},
-// })
-// const filterReducer = createReducer('', {
-//   [setFilter]: (state, action) => {return action.payload;},
-// })
 
 export const store = configureStore({
     reducer: {
        contacts: contactReducer,
        filter: filterReducer
-      //contacts: contactsSlice.reducer,
-      //filter: filterSlice.reducer
     },
   });
-  
-  // export const {addClient, delClient } = contactsSlice.actions;
-  //export const {setFilter} = filterSlice.actions;
