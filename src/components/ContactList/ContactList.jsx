13e4c6fd85037def1e0ff063ee8d8dbd26afbc6e
IgorMushk <igor.mushketik@gmail.com>
2023-10-06 19:10:28 +0300

    Add selectors.js (for ContactList.js)

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,12 +2,15 @@ import { Button, Item, ItemText, List } from "./ContactList.styled.";
 import { useDispatch, useSelector } from 'react-redux';
 //import { delClient } from "redux/store";
 import { delClient } from "redux/contactsSlice";
+import { getContacts, getFilterValue } from "redux/selectors";
 
 export const ContactList = ({onDeleteContact}) => {
-  const contacts  = useSelector(state => state.contacts)
+  //const contacts  = useSelector(state => state.contacts)
+  const contacts  = useSelector(getContacts)
   console.log('ContactList-contacts :>>', contacts)
   const dispatch = useDispatch();
-  const filterValue = useSelector(state => state.filter);
+  //const filterValue = useSelector(state => state.filter);
+  const filterValue = useSelector(getFilterValue);
   console.log('ContactList-filter :>>', filterValue);
 
   const getFilteredContats = () => {
